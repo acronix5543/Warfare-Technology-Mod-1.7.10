d4b93daecee9b546fd3d9aaed08ffaf3e4f71ad9
Warfare Technology  Mod <78573717+WarTechMod@users.noreply.github.com>
2023-08-30 16:43:08 +0200

    ,ol

--- src/main/java/com/wartec/wartecmod/entity/missile/EntityMissileSlbm.java
+++ src/main/java/com/wartec/wartecmod/entity/missile/EntityMissileSlbm.java
@@ -1,6 +1,7 @@
 package com.wartec.wartecmod.entity.missile;
 
 import com.hbm.entity.effect.EntityNukeCloudSmall;
+import com.hbm.entity.effect.EntityNukeTorex;
 import com.hbm.entity.logic.EntityNukeExplosionMK5;
 import com.hbm.items.ModItems;
 import com.wartec.wartecmod.items.wartecmodItems;
@@ -26,30 +27,15 @@ public class EntityMissileSlbm extends EntityBallisticMissileBase {
 		loadNeighboringChunks((int)(posX / 55), (int)(posZ / 55));
 		//1. Sprengkopf
     	worldObj.spawnEntityInWorld(EntityNukeExplosionMK5.statFac(worldObj, 150, posX+50, posY, posZ)); //St�rke 250
-
-		EntityNukeCloudSmall entity2 = new EntityNukeCloudSmall(this.worldObj, 1000, 150 * 0.005F);//250
-    	entity2.posX = this.posX+50;
-    	entity2.posY = this.posY;
-    	entity2.posZ = this.posZ;
-    	this.worldObj.spawnEntityInWorld(entity2);
+		EntityNukeTorex.statFac(worldObj, posX+50, posY, posZ, 150);
     	
     	//2.Sprengkopf
     	worldObj.spawnEntityInWorld(EntityNukeExplosionMK5.statFac(worldObj, 150, posX-50, posY, posZ+50)); //St�rke 250
-
-		EntityNukeCloudSmall entity3 = new EntityNukeCloudSmall(this.worldObj, 1000, 150 * 0.005F);//250
-    	entity3.posX = this.posX-50;
-    	entity3.posY = this.posY;
-    	entity3.posZ = this.posZ+50;
-    	this.worldObj.spawnEntityInWorld(entity3);
+		EntityNukeTorex.statFac(worldObj, posX-50, posY, posZ+50, 150);
     	
     	//3.Sprengkopf
     	worldObj.spawnEntityInWorld(EntityNukeExplosionMK5.statFac(worldObj, 150, posX-50, posY, posZ-50)); //St�rke 250
-
-		EntityNukeCloudSmall entity4 = new EntityNukeCloudSmall(this.worldObj, 1000, 150 * 0.005F);//250
-    	entity4.posX = this.posX-50;
-    	entity4.posY = this.posY;
-    	entity4.posZ = this.posZ-50;
-    	this.worldObj.spawnEntityInWorld(entity4);
+		EntityNukeTorex.statFac(worldObj, posX-50, posY, posZ-50, 150);
 	}
 
 	@Override
